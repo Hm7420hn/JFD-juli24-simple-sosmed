15b7b6ff8c22714ff8caca021fe02e90773667e5
Hm7420hn <7mo0ody7m105690@gmail.com>
2024-08-19 21:19:31 +0700

    membuat form login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express   = require('express')
 const app       = express()
 const port      = 3000
 const c_beranda = require('./controller/c_beranda')
+const c_auth = require('./controller/c_auth')
 
 
 app.use( express.urlencoded({extended:false}) )
@@ -12,6 +13,7 @@ app.set('views', './view')
 
 
 app.get('/', c_beranda.index)
+app.get('/login', c_auth.form_login)
 
 
 app.listen(port, ()=>{
