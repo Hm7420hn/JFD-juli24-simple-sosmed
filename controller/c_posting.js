cc5a78b8bceb28a60f9e0370c8a271a4d663fdda
Hm7420hn <7mo0ody7m105690@gmail.com>
2024-08-26 21:45:48 +0700

    form buat postingan baru

--- controller/c_posting.js
+++ controller/c_posting.js
@@ -1,4 +1,4 @@
-const m_user   = require('./../model/m_user')
+const m_post   = require('./../model/m_post')
 const path     = require('path') 
 const moment   = require('moment')
 moment.locale('id')
@@ -12,5 +12,50 @@ module.exports =
             message: req.session.msg,
         }
         res.render('posting/index', dataview)
-    }
+    },
+
+
+    proses_insert: async function(req,res){
+        let caption = req.body.form_caption
+        let media1  = req.files.form_media1
+        let media2  = req.files.form_media2
+        let media3  = req.files.form_media3
+
+
+        if (caption || (media1 || media2 || media3) ) {
+            // proses insert ke database
+            try {
+                
+                let max_size= 1024 * 1024 * 3 //3MB
+                
+                // cek ukuran media
+                if (media1.size > max_size) {
+                    return res.redirect(`/posting?msg-Media 1 exceeds size melebihi bata limit 3MB`)    
+
+                } else if (media2.size > max_size) {
+                    return res.redirect(`/posting?msg-Media 1 exceeds size melebihi bata limit 3MB`)    
+        
+                } else if (media3.size > max_size) {
+                    return res.redirect(`/posting?msg-Media 1 exceeds size melebihi bata limit 3MB`)    
+
+                } else {
+                    // proses insert ke database
+                    let insert = await m_post.insert(req)
+                    if (insert.affectedRows > 0) {
+                        return res.redirect(`/feed?msg-berhasil kirim postingan`)
+                    }
+                    
+                }    
+            } catch (error) {
+                // menangkap error dari proses try (insert ke db)
+                console.log(error);
+                res.redirect(`/posting?msg${error}`)
+            }
+        } else {
+            // kirim pesan error/warning
+            // terhadap pengecekan antara caption atau media, salah satunya harus terisi
+            let pesan_error = 'Caption atau Media harus terisi salah satu'
+            res.redirect(`/posting?msg=${pesan_error}`)
+        }
+    },
 }
